22564ecc7e0a62108dc4f7b5bfa18de2fe668058
Alana714 <142412727+Alana714@users.noreply.github.com>
2025-03-06 22:11:39 -0300

    Add files via upload

---
+++ Atividade 1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let numA = 7;
+let numB = 4;
+let resultado; 
+
+resultado = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${resultado}`);
+
+resultado = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${resultado}`);
+
+resultado = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${resultado}`);
+
+resultado = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${resultado}`);

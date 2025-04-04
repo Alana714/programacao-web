ef015ce11438fb6b4fb15810d9fee5298e2118cd
Alana714 <142412727+Alana714@users.noreply.github.com>
2025-04-04 20:48:57 -0300

    Update index.js

--- atividade4/index.js
+++ atividade4/index.js
@@ -1,14 +1,14 @@
 const express = require('express'); //importa o exp
 const app = express(); // faz o uso do exp
-const estoque = require('./estoque')//carrega funções q estao no export
+const estoque = require('./estoque');//carrega funções q estao no export
 
 app.get('/', (req,res)=>{
     let html = '<h1>app_estoque</h1>';
     html += '<h3> rotas disponiveis: </h3>';
-    html += '<p>/adicionar/:id/:nome/:qtd</p>'
-    html += '<p></listar/p>'
-    html += '<p>/remover/:id</p>'
-    html += '<p>/editar/:id/:qtd</p>'
+    html += '<p>/adicionar/:id/:nome/:qtd</p>';
+    html += '<p>/listar</p>';
+    html += '<p>/remover/:id</p>';
+    html += '<p>/editar/:id/:qtd</p>';
     res.send(html);
 }); //qual rota vai ser atendida, sempre precisa de req e resposta; quando recebe /, manda tal resp.
 
@@ -27,17 +27,19 @@ app.get('/listar', (req,res)=>{
 });
 
 app.get('/remover/:id', (req,res)=>{
+    let id= Number(req.params.id);
 
+    res.send(estoque.remover(id));
 });
 
 app.get('/editar/:id/:qtd', (req,res)=>{
     let id= Number(req.params.id);
     let qtd = Number(req.params.qtd);
 
-    res.send(estoque.editar(item));
+    res.send(estoque.editar(id,qtd));
 });
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
-    console.log('app rodando na porta' + PORT);
+    console.log('app rodando na porta: ' + PORT);
 }); // escuta req nessa porta
